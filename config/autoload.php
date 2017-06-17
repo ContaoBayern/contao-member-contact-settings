47c3c486ac63fa418031a9155357607bef44f5fa
fiedsch <fiedsch@ja-eh.at>
2017-06-17 14:38:56 +0200

    add own registration template (close #7)

--- config/autoload.php
+++ config/autoload.php
@@ -1,28 +1,21 @@
 <?php
 
 /**
- * Contao Open Source CMS
- *
- * Copyright (c) 2005-2017 Leo Feyer
- *
- * @license LGPL-3.0+
- */
-
-
-/**
  * Register the namespaces
  */
-ClassLoader::addNamespaces(array
-(
-	'ContaoBayern',
-));
+ClassLoader::addNamespaces([
+    'ContaoBayern',
+]);
 
 
 /**
  * Register the classes
  */
-ClassLoader::addClasses(array
-(
-	// Modules
-	'ContaoBayern\MemberContactSettings\Modules\ModuleRegistration' => 'system/modules/member-contact-settings/modules/ModuleRegistration.php',
-));
+ClassLoader::addClasses([
+    // Modules
+    'ContaoBayern\MemberContactSettings\Modules\ModuleRegistration' => 'system/modules/member-contact-settings/modules/ModuleRegistration.php',
+]);
+
+TemplateLoader::addFiles([
+    'member_default' => 'system/modules/member-contact-settings/templates/member',
+]);
