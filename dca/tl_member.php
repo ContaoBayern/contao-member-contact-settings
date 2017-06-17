efb733204e97bfd689555a337876d629bba64f93
fiedsch <fiedsch@ja-eh.at>
2017-06-17 15:31:08 +0200

    edit authors

--- dca/tl_member.php
+++ dca/tl_member.php
@@ -4,7 +4,8 @@
  * Member contact settings extension for Contao Open Source CMS.
  *
  * @copyright  Copyright (c) 2017 Contao Bayern
- * @author     Contao Bayern
+ * @author     Jörg Moldenhauer
+ * @author     Andreas Fieger
  * @license    https://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  *
  * @see        https://github.com/ContaoBayern/contao-member-contact-settings
