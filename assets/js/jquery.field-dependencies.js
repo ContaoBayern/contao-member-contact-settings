6cfaa8cd7f86e1954602007424f10793fc6904da
Jörg Moldenhauer <joergmoldenhauer@users.noreply.github.com>
2017-07-09 11:59:36 +0200

    Replace ambiguous "element" with "input" (see #4)

--- assets/js/jquery.field-dependencies.js
+++ assets/js/jquery.field-dependencies.js
@@ -11,24 +11,24 @@
          *
          * @constructor
          *
-         * @param element object The input field element (jQuery object)
+         * @param input object The input field (jQuery object)
          */
-        function Field(element) {
+        function Field(input) {
             var self = this;
 
-            self.element = element;
+            self.input = input;
 
-            self.label = self.element.siblings('label');
+            self.label = self.input.siblings('label');
             self.originalLabelText = self.label.html();
 
-            self.widget = self.element.parents('.widget');
+            self.widget = self.input.parents('.widget');
         }
 
         /**
          * Sets the field to be mandatory.
          */
         Field.prototype.setMandatory = function () {
-            this.element.prop('required', true);
+            this.input.prop('required', true);
             this.label.html(this.originalLabelText + config.mandatoryLabelAdditon);
         };
 
@@ -36,7 +36,7 @@
          * Removes the fields mandatory status.
          */
         Field.prototype.removeMandatoryStatus = function () {
-            this.element.prop('required', false);
+            this.input.prop('required', false);
             this.label.html(this.originalLabelText);
         };
 
@@ -44,7 +44,7 @@
          * Shows the field.
          */
         Field.prototype.show = function () {
-            this.element.prop('disabled', false);
+            this.input.prop('disabled', false);
             this.widget.show();
         };
 
@@ -52,7 +52,7 @@
          * Hides the field.
          */
         Field.prototype.hide = function () {
-            this.element.prop('disabled', true);
+            this.input.prop('disabled', true);
             this.widget.hide();
         };
 
@@ -68,29 +68,29 @@
         function FieldWithDependencies(form, name, dependencies) {
             var self = this;
 
-            self.element = form.find('input[name="' + name + '"]');
+            self.input = form.find('input[name="' + name + '"]');
 
             self.fieldsToSetMandatory = [];
             $.each(dependencies.mandatory, function (index, fieldName) {
-                var element = form.find('[name="' + fieldName + '"]');
+                var input = form.find('[name="' + fieldName + '"]');
                 // Only add fields which are not mandatory by default
-                if (element.attr('required') === undefined) {
-                    self.fieldsToSetMandatory.push(new Field(element));
+                if (input.attr('required') === undefined) {
+                    self.fieldsToSetMandatory.push(new Field(input));
                 }
             });
 
             self.fieldsToShowOrHide = [];
             $.each(dependencies.visibility, function (index, fieldName) {
-                var element = form.find('[name="' + fieldName + '"]');
+                var input = form.find('[name="' + fieldName + '"]');
                 // Only add fields which are not mandatory by default
-                if (element.attr('required') === undefined) {
-                    self.fieldsToShowOrHide.push(new Field(element));
+                if (input.attr('required') === undefined) {
+                    self.fieldsToShowOrHide.push(new Field(input));
                 }
             });
 
             self.hideDependentFields();
 
-            self.element.on('change', function () {
+            self.input.on('change', function () {
                 if ($(this).is(':checked')) {
                     self.setDependentFieldsMandatory();
                     self.showDependentFields();
